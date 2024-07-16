bb53a7b4c89e82236ceaa4189f2ba51ff74cbaae
yaoyc <yaoyunchou@bananain.com>
2024-07-16 12:32:00 +0800

    feat(env): ✨ 配置

--- ormconfig.ts
+++ ormconfig.ts
@@ -18,7 +18,9 @@ export function getServerConfig() {
   const envConfig = getEnv(`.env.${process.env.NODE_ENV || 'development'}`);
   // configService
   const config = { ...defaultConfig, ...envConfig };
-  return config;
+  console.log('config-------------------------', config)
+  console.log('process.env-------------------------', process.env)
+  return process.env.NODE_ENV ==="production" ? process.env : config;
 }
 
 // 通过dotENV来解析不同的配置
